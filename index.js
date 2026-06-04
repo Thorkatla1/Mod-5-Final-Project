fee7510720371012064cd77f1a243a48afa36beb
Thorkatla1 <thorkatla@yahoo.com>
2026-06-03 21:03:47 -0400

    working to move header to corner on click

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log('hello world');
 
 function shrinkImg(event) {
-    
-
+    const img = document.querySelector('.header__img');
+    img.style.width = '50%';
 }
+let img = document.createElement('img');
+img.classList.add('header__container');
