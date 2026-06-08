577e8970ddf83f17b27e12f84584a5f79cbd3b85
Thorkatla1 <thorkatla@yahoo.com>
2026-06-08 12:15:58 -0400

    trying to remove sky from corner of first page

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log('hello world');
 
 function shrinkImg(event) {
-    const img = document.querySelector('.header__img--wrapper');
-    img.style.width = '20%';
+    const img = document.querySelector('.main__header--wrapper img');
+    img.style.width = '18%';
+}
+if (window.location.pathname === '/page2') {
+    document.body.classList.add('page-2__img--text');
 }
