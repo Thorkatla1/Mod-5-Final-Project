511de33f0839527303fc9f9702aea245bf389aef
Thorkatla1 <thorkatla@yahoo.com>
2026-06-09 02:59:52 -0400

    prettier error line 31.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 console.log('hello world');
 
-function shrinkImg(event) {
-    const img = document.querySelector('.main__header--wrapper img');
-    img.style.width = '18%';
-}
-if (window.location.pathname === '/page2') {
-    document.body.classList.add('page-2__img--text');
-}
+
