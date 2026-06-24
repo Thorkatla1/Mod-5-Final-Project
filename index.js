0cf304dc1a5565ffeb27d190bf13b5f4a09ef29b
Thorkatla1 <thorkatla@yahoo.com>
2026-06-24 17:26:43 -0400

    back to start on index.js - k.i.s.s.

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 console.log("hello world");
 
-async function main(){
-const users = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=3a3dec69"),
-const usersData = users.json();
-console.log(userData)
-
-
-}
-main();
+console.log("https://www.omdbapi.com/?i=tt3896198&apikey=3a3dec69")
