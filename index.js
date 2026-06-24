99f250641106937858f879f252da1d2be548ff9a
Thorkatla1 <thorkatla@yahoo.com>
2026-06-24 16:51:47 -0400

    discovered only need one window in html

--- index.js
+++ index.js
@@ -1,60 +1,10 @@
 console.log("hello world");
 
-
-// connect api
-
-
-
-
-// link button to render movies
-
-
-
-
-let movies;
-
-async function renderMovies(filter) {
-  const moviesWrapper = document.querySelector(".movies");
-
-  moviesWrapper.classList += ' movie__loading'
-
-  if (!movies) {
-    movies = await getMovies();
-  }
-  
-  moviesWrapper.classList.remove('movies__loading')
-
-  if (filter === "A_TO_Z") {
-    movies.sort(
-      (a, b) =>
-        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
-    );
-  } else if (filter === "Z_TO_A") {
-    movies.sort(
-      (a, b) =>
-        (b.salePrice || b.originalPrice) - (a.salePrice || a.originalPrice)
-    );
-  } else if (filter === "RATING") {
-    movies.sort((a, b) => b.rating - a.rating);
-  }
-
-
-function filterMovies(event) {
-  renderMovies(event.target.value);
-}
-
-setTimeout(() => {
-  renderMovies();
-})
-
-
-
-
+async function main(){
+const users = await fetch("http://www.omdbapi.com/?i=tt3896198&apikey=3a3dec69"),
+const usersData = users.json();
+console.log(userData)
 
 
 }
-
-
-
-
-
+main();
