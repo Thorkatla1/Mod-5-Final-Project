a9e0298d465601d2f54449597cf2cf1a0609c547
Thorkatla1 <thorkatla@yahoo.com>
2026-07-15 13:32:03 -0400

    sort filter not working

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ async function main() {
     "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies-2020s.json",
   );
   const movieData = await response.json();
-  const movies = movieData.slice(0, 6);
+  const movies = movieData.slice(0, 50);
   console.log(movies);
 }
 main();
@@ -25,26 +25,23 @@ function filterMovies(filter) {
   } else if (filter === "Z TO A") {
     movies.sort((a, b) => b.title.localeCompare(a.title));
   } else if (filter === "OLDEST TO NEWEST") {
-    movie.sort((a, b) => a.year - b.year);
+    year.sort((a, b) => new year(a) - new year(b));
   } else if (filter === "NEWEST TO OLDEST") {
     movie.sort((a, b) => b.year - a.year);
   }
+
   async function renderMovies(filter) {
     const moviesWrapper = document.querySelector(".movies");
     moviesWrapper.classList.add("movies__loading");
-
     if (!movies) {
-      movies = await getmovies();
+      movies = await getMovies();
     }
-
     moviesWrapper.classList.remove("movies__loading");
   }
 }
+
 function displayMovie(movie) {
-  console.log(movie.Title);
-  console.log(movie.Year);
+  console.log(movie.title);
+  console.log(movie.year);
   console.log(movie.Poster);
 }
-
-main();
-// console.log(movieData);
