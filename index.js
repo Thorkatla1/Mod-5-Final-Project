593ad5121392463b697e852c51de057275ab2266
Thorkatla1 <thorkatla@yahoo.com>
2026-07-16 10:36:17 -0400

    working on poster

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 console.log("hello world");
+const posterRef = document.querySelector(".poster");
+console.log(posterRef);
 
 async function main() {
   const response = await fetch(
-    "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies-2020s.json",
+    "https://www.omdbapi.com/?i=tt3896198&apikey=3a3dec69&",
   );
   const movieData = await response.json();
-  const movies = movieData.slice(0, 50);
-  console.log(movies);
+  posterRef.innerHTML = movieData.Poster;
+  // const movies = movieData.slice(0, 50);
+  console.log(movieData);
 }
 main();
+
+// async function main() {
+//   const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=3a3dec69");
+//   const movieData = await response.json();
+//   const movies = movieData.slice(0, 50);
+
+  const moviesHtml = movies
+    .map((movie) => {
+      return `
+        <div class="movie">
+          <h3>${movie.poster}</h3>
+        </div>
+      `;
+    })
+    .join("");
+
+//   document.querySelector(".movies__list").innerHTML = moviesHtml;
+// }
+
+// main();
 
 async function renderMovies(filter) {
   const moviesWrapper = document.querySelector(".movies");
@@ -21,27 +44,30 @@ async function renderMovies(filter) {
 
 function filterMovies(filter) {
   if (filter === "A TO Z") {
-    movies.sort((a, b) => a.title.localeCompare(b.title));
+    movies.sort((a, b) => a.Title.localeCompare(b.Title));
   } else if (filter === "Z TO A") {
-    movies.sort((a, b) => b.title.localeCompare(a.title));
+    movies.sort((a, b) => b.Title.localeCompare(a.Title));
   } else if (filter === "OLDEST TO NEWEST") {
-    year.sort((a, b) => new year(a) - new year(b));
+    movies.sort((a, b) => a.Year - b.Year); // Ensure you're sorting based on the Year property
   } else if (filter === "NEWEST TO OLDEST") {
-    movie.sort((a, b) => b.year - a.year);
+    movies.sort((a, b) => b.Year - a.Year);
+  } else {
+    console.warn("Filter option not recognized");
   }
+  console.log(filterMovies);
+}
 
-  async function renderMovies(filter) {
-    const moviesWrapper = document.querySelector(".movies");
-    moviesWrapper.classList.add("movies__loading");
-    if (!movies) {
-      movies = await getMovies();
-    }
-    moviesWrapper.classList.remove("movies__loading");
+async function renderMovies(filter) {
+  const moviesWrapper = document.querySelector(".movies");
+  moviesWrapper.classList.add("movies__loading");
+  if (!movies) {
+    movies = await getMovies();
   }
+  moviesWrapper.classList.remove("movies__loading");
 }
 
 function displayMovie(movie) {
-  console.log(movie.title);
-  console.log(movie.year);
+  console.log(movie.Title);
+  console.log(movie.Year);
   console.log(movie.Poster);
 }
