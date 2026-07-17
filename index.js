7d86aa74a60a2bb067a1f1ed6e8b2779bd073739
Thorkatla1 <thorkatla@yahoo.com>
2026-07-17 16:57:50 -0400

    help me Frank

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
 console.log("hello world");
+
+const moviesList = document.querySelector(".movies__list");
 // const posterRef = document.querySelector(".poster");
 // console.log(posterRef);
-
+let movies = [];
 async function main(movieName) {
   const response = await fetch(
     `https://www.omdbapi.com/?s=${movieName}&apikey=3a3dec69`,
   );
-  console.log(movieName)
-  const movieData = await response.json();
+  console.log(movieName);
+  movieData = await response.json();
   console.log(movieData);
-  const movies = movieData.Search.slice(0, 6);
+  movies = movieData.Search.slice(0, 6);
 
-  document.querySelector(".movies__list").innerHTML = movies
-    .map((movie) => {
-      return `<img class = "img__poster" src="${movie.Poster}" alt="${movie.Title}">`;
-    })
-    .join("");
-  // Q: how do i know this will show 10 in the console? 
+
+  // Q: how do i know this will show 10 in the console?
   // A:  this individual api is coded to show only 10 in the console.
 }
 
@@ -25,20 +23,22 @@ function inputData(event) {
   console.log(event.target.value);
 }
 
-// function filterMovies(filter) {
-//   if (filter === "A TO Z") {
-//     movies.sort((a, b) => a.Title.localeCompare(b.Title));
-//   } else if (filter === "Z TO A") {
-//     movies.sort((a, b) => b.Title.localeCompare(a.Title));
-//   } else if (filter === "OLDEST TO NEWEST") {
-//     movies.sort((a, b) => a.Year - b.Year); // Ensure you're sorting based on the Year property
-//   } else if (filter === "NEWEST TO OLDEST") {
-//     movies.sort((a, b) => b.Year - a.Year);
-//   } else {
-//     console.warn("Filter option not recognized");
-//   }
-//   console.log(filterMovies);
-// }
+function filterMovies(event) {
+  const filter = event.target.value;
+  console.log(filter);
+  if (filter === "A_TO_Z") {
+    movies.sort((a, b) => a.Title.localeCompare(b.Title));
+  } else if (filter === "Z_TO_A") {
+    movies.sort((a, b) => b.Title.localeCompare(a.Title));
+  } else if (filter === "OLDEST_TO-NEWEST") {
+    movies.sort((a, b) => a.Year - b.Year); // Ensure you're sorting based on the Year property
+  } else if (filter === "NEWEST_TO_OLDEST") {
+    movies.sort((a, b) => b.Year - a.Year);
+  } else {
+    console.warn("Filter option not recognized");
+  }
+  console.log(filterMovies);
+}
 
 async function renderMovies(filter) {
   const moviesWrapper = document.querySelector(".movies");
@@ -47,6 +47,11 @@ async function renderMovies(filter) {
     movies = await getMovies();
   }
   moviesWrapper.classList.remove("movies__loading");
+    document.querySelector(".movies__list").innerHTML = movies
+    .map((movie) => {
+      return `<img class = "img__poster" src="${movie.Poster}" alt="${movie.Title}">`;
+    })
+    .join("");
 }
 
 function displayMovie(movie) {
